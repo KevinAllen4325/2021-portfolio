dbc9559acffa5eef25d1780a8dc374bf89d8b374
Kevin O'Neill <kevinallenoneill@gmail.com>
2022-03-30 11:05:45 -0500

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
 import '../styles/app.scss'
+import { useEffect } from "react";
+import { useRouter } from 'next/router';
+
 
 function MyApp({ Component, pageProps }) {
+  const handleRouteChange = (url) => {
+    window.gtag('config', '[Tracking ID]', {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,26 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-117107118-1"
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'UA-117107118-1', { page_path: window.location.pathname });
+              `,
+            }}
+          />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />  
-          <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;600&family=Montserrat:wght@400;700&display=swap" rel="preload" as="style" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&family=Montserrat:wght@400;700&display=swap" media="all" onLoad="this.media='all'"></link>
+          <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;600&family=Montserrat:wght@400;700&display=swap" rel="preload" as="stylesheet" />
+          
         </Head>
         <body>
           <Main />
